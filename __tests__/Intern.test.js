44787e8700479f189ca6a55e64b77799f4e15f6a
SafteyDav3 <talktomedw@gmail.com>
2022-07-09 00:25:48 -0500

    Intern test created

---
+++ __tests__/Intern.test.js
@@ -0,0 +1,28 @@
+// Importing Intern Class Object
+const Intern = require("../lib/Intern");
+
+test("Creates Intern...", () => {
+  // Create a new Intern
+  const intern = new Intern("Jerry Jones", 1, "jerryjones@who.com", "UWGB");
+
+  expect(intern.name).toBe("Jerry Jones");
+  expect(intern.id).toBe(1);
+  expect(intern.email).toBe("jerryjones@who.com");
+  expect(intern.school).toBe("UWGB");
+});
+
+test("Fetching School attended from 'Intern'.", () => {
+  // Create a new Intern
+  const intern = new Intern("Jerry Jones", 1, "jerryjones@who.com", "whoHub");
+  const internSchool = intern.fetchSchool();
+
+  expect(internSchool).toBe("UWGB");
+});
+
+test("Testing to make sure employee role is 'Intern'.", () => {
+  // Create a new Intern
+  const intern = new Intern("Jerry Jones", 1, "jerryjones@who.com", "whoHub");
+  const internRole = intern.fetchRole();
+
+  expect(internRole).toBe("Intern");
+});
